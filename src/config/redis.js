8c7c6fdc88fca1e2569a079d8afcaea73b42f764
Hnaa17 <farhanaachmad8578@gmail.com>
2022-10-30 11:30:31 +0700

    upload photo

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,6 +1,15 @@
 const { createClient } = require('redis');
 
-const client = createClient(6379);
+const client = redis.createClient({
+    url: 'redis://redis:6379'
+});
+
+client.connect();
+
+client.on('connect', (err) => {
+    if(err) throw err;
+    else console.log('Redis Connected..!');
+});
 
 client.on('error', (err) => console.log('Redis Client Error', err));
 
